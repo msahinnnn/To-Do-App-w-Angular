e2e27c987bf4b2f1fda863a6bd44f92bb87b8a76
msahinnnn <mehmetsahin.99@outlook.com>
2022-05-21 20:23:17 +0300

    Angular implementation and basic exercises.

---
+++ todoapp/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+
+import { TodoComponent } from './todo/todo.component';
+
+
+@NgModule({
+  declarations: [
+    TodoComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [TodoComponent]
+})
+export class AppModule { }
